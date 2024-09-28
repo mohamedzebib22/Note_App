6ffb41e58adb12eb4e345615c0019d6e64bac30b
mohamedzebib22 <mohamed_zebib@yahoo.com>
2024-09-29 01:22:51 +0300

    create custom appbar and custom seach icon

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -7,7 +7,7 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NotesViewBody(),
+      body:  NotesViewBody(),
     );
   }
 }
