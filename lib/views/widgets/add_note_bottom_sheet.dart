e7d07d081b14a1355aa9790a702f998ffca019fb
mohamedzebib22 <mohamed_zebib@yahoo.com>
2024-10-02 01:00:15 +0300

    finish add notes to buttom sheet

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widgets/custom_button.dart';
 import 'package:note_app/views/widgets/custom_text_feild.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -6,15 +7,26 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return const Padding(
       padding: const  EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32,),
-          CustomTextFeild(hint: 'title',),
-          SizedBox(height: 20,),
-          CustomTextFeild(hint: 'content' , maxLines: 5,),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32,),
+
+            CustomTextFeild(hint: 'title',),
+
+            SizedBox(height: 20,),
+
+            CustomTextFeild(hint: 'content' , maxLines: 5,),
+
+            SizedBox(height: 40,),
+
+            CustomButton(),
+
+            SizedBox(height: 16,)
+          ],
+        ),
       ),
     );
   }
