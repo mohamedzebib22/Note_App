dab908f89aa0e03f5142498c294baae95c0285fa
mohamedzebib22 <mohamed_zebib@yahoo.com>
2024-10-01 16:00:15 +0300

    Create Custom TextField

---
+++ lib/views/widgets/custom_text_feild.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/constans.dart';
+
+class CustomTextFeild extends StatelessWidget {
+  const CustomTextFeild({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+        hintText: 'Tilte',
+        hintStyle: const TextStyle(color: kPrimaryColor),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide: BorderSide(
+          color: color ?? Colors.white
+        )
+      );
+  }
+}
